b5a4acfdb1dc5baf67be19217a40acb8668d36ad
rjb255 <62777572+rjb255@users.noreply.github.com>
2021-03-06 12:10:12 +0000

    reorganised

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-data = pd.read_excel("Digital_Data.xlsx")
-data.drop(["Project Ref No."], axis = 1)
-print(data.head())
-
 #x is the data given, y to be predicted, train, valid, and test are the fractions to be trained, validated, and tested on
 def split(x, y, train = 0.8, valid = 0): 
     test = 1 - train - valid
@@ -14,6 +10,10 @@ def split(x, y, train = 0.8, valid = 0):
     x_train, x_valid, y_train, y_valid = train_test_split(x_trainy, y_trainy, train/(train+valid),valid/(train+valid))
 
     return x_train, x_valid, x_test, y_train, y_valid, y_test
+
+data = pd.read_excel("Digital_Data.xlsx")
+data.drop(["Project Ref No."], axis = 1)
+print(data.head())
 
 if (__name__ == "__main__"):
     pass
